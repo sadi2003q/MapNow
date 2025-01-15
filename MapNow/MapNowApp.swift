25f46c8608d3b26d8d93d33e000834bd158fd95f
Md. Adnan Abdullah <Adnanabdullah625@gmail.com>
2025-01-15 14:30:28 +0600

    [Code start]

---
+++ MapNow/MapNowApp.swift
@@ -0,0 +1,32 @@
+//
+//  MapNowApp.swift
+//  MapNow
+//
+//  Created by  Sadi on 15/01/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MapNowApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MapNowTests/MapNowTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapNowTests.swift
+//  MapNowTests
+//
+//  Created by  Sadi on 15/01/2025.
+//
+
+import Testing
+@testable import MapNow
+
+struct MapNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
